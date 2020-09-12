4c9cc5927dbec48e875b3a8e2258fc7b0600d032
JarmovanDongen <55130148+JarmovanDongen@users.noreply.github.com>
2020-09-13 01:08:02 +0200

    Player Controller Added (Not Balanced yet)
    The player is added but he needs to be balanced a little bit more. I have to do that when the map is made so I can check how high he should be jumping and how to walk on steps.

---
+++ Brainless/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += -transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += -transform.forward * Time.deltaTime * speed;
+        }
+    }
+}
