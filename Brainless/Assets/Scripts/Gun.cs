05e2fc2cee812583b1a89c37beca4479793172ae
JarmovanDongen <55130148+JarmovanDongen@users.noreply.github.com>
2020-09-23 17:56:46 +0200

    Leafblower mechanic added

---
+++ Brainless/Assets/Scripts/Gun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public float force = 50f;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        Rigidbody2D rigid = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (player != null)
+        {
+            rigid.AddForce(new Vector3(force, force, 0));
+        }
+    }
+}
---
+++ Brainless/Assets/Shooting.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public int shootingStrength;
+    public Vector3 shootingPosition;
+    public GameObject leafBlower;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            shooting();   
+        }
+    }
+
+    void shooting()
+    {
+        Quaternion leafBlowerRotation =leafBlower.transform.rotation * Quaternion.Euler(90,0,0);
+        shootingPosition = leafBlower.transform.position + (leafBlowerRotation * new Vector3(0, 0, 3));
+        RaycastHit hit;
+        Vector3 direction = leafBlowerRotation * new Vector3(0,0,1);
+        
+        Debug.DrawRay(shootingPosition, direction, Color.red, 5000);
+        bool hasCollided = Physics.Raycast(shootingPosition, direction, out hit, 100, LayerMask.NameToLayer("LeafBlower"));
+        
+        if (hasCollided)
+        {
+            GameObject zombie = hit.collider.gameObject;
+          
+            Vector3 shootingDirection = zombie.transform.position - shootingPosition ;
+            Debug.Log(zombie.transform.position);
+            Debug.DrawRay(shootingPosition, shootingDirection, Color.red, 5);
+            Vector3 shootingForce = shootingDirection.normalized * shootingStrength;
+            zombie.GetComponent<Rigidbody>().AddForce(shootingForce, ForceMode.Impulse);
+        }
+
+    }
+}
