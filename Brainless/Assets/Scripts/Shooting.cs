691287691d69727d4d1767ea200faf09c649a2e6
JarmovanDongen <55130148+JarmovanDongen@users.noreply.github.com>
2020-09-25 03:38:01 +0200

    Enemy damage added and healthbar

--- Brainless/Assets/Scripts/Shooting.cs
+++ Brainless/Assets/Scripts/Shooting.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ public class Shooting : MonoBehaviour
     public int shootingStrength;
     public Vector3 shootingPosition;
     public GameObject leafBlower;
+    public int shootingRange = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,23 +28,21 @@ public class Shooting : MonoBehaviour
     void shooting()
     {
         Quaternion leafBlowerRotation =leafBlower.transform.rotation * Quaternion.Euler(90,0,0);
-        shootingPosition = leafBlower.transform.position + (leafBlowerRotation * new Vector3(0, 0, 3));
+        shootingPosition = leafBlower.transform.position + (leafBlowerRotation * new Vector3(0, 0, 0));
         RaycastHit hit;
         Vector3 direction = leafBlowerRotation * new Vector3(0,0,1);
         
         Debug.DrawRay(shootingPosition, direction, Color.red, 5000);
-        bool hasCollided = Physics.Raycast(shootingPosition, direction, out hit, 100, LayerMask.NameToLayer("LeafBlower"));
+        bool hasCollided = Physics.Raycast(shootingPosition, direction, out hit, shootingRange, LayerMask.NameToLayer("LeafBlower"));
         
         if (hasCollided)
         {
             GameObject zombie = hit.collider.gameObject;
-          
-            Vector3 shootingDirection = zombie.transform.position - shootingPosition ;
-            Debug.Log(zombie.transform.position);
-            Debug.DrawRay(shootingPosition, shootingDirection, Color.red, 5);
-            Vector3 shootingForce = shootingDirection.normalized * shootingStrength;
-            zombie.GetComponent<Rigidbody>().AddForce(shootingForce, ForceMode.Impulse);
+            zombie.GetComponent<ZombieHealth>().TakeDamage(shootingPosition, shootingStrength);
+
         }
 
     }
+
+
 }
