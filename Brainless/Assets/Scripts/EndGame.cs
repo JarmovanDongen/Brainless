67ea9c59f2124fef8eefbbe4416bb5680f545d3a
JarmovanDongen <55130148+JarmovanDongen@users.noreply.github.com>
2023-01-17 15:09:44 -0800

    A lot

---
+++ Brainless/Assets/Scripts/EndGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Eindelijk");
+            SceneManager.LoadScene("Victory", LoadSceneMode.Single);
+        }
+    }
+}
